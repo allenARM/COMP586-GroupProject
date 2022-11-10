fbe6763e1f8ae5c1f5b99f9cb54f450794f74c38
nimbus <allenwem@gmail.com>
2022-11-09 23:18:36 -0800

    removing interfaces

--- 586FinalProject/Program.cs
+++ 586FinalProject/Program.cs
@@ -2,11 +2,12 @@
 
 int main()
 {
-    IDevice speaker = new Speaker();
-    IDevice light = new Light();
+    Speaker speaker = new Speaker();
+    Light light = new Light();
 
     speaker.getLocation();
-    speaker.setVolume();
+    speaker.getVolume();
+    speaker.setVolume(10);
 
     Controller controller = new Controller();
 
@@ -21,29 +22,49 @@ main();
 
 public class Controller
 {
-    List<IDevice> devices = new List<IDevice>();
+    List<Object> devices = new List<Object>();
 
-    public List<IDevice> getDevices() { return devices; }
+    //List<Room> rooms = new List<Room>();
 
-    private Boolean checkIfExists(IDevice device)
+    public List<Object> getDevices() { return devices; }
+
+    private Boolean checkIfExists(Object device)
     {
-        foreach (IDevice d in devices)
+        foreach (Object d in devices)
         {
-            if (d.GetType() == device.GetType())
+            if (d.GetType() == d.GetType())
                 return true;
         }
         return false;
     }
 }
 
-public interface IDevice
+public class Room
 {
-    public string getLocation();
-    public void setLocation(string location);
-
-    public Boolean getStatus();
-    public void setStatus(Boolean status);
+    List<Object> devices = new List<Object>();
+    private string location;
 }
+
+//public interface IDevice: ISpeaker, ILight
+//{
+//    public string getLocation();
+//    public void setLocation(string location);
+
+//    public Boolean getStatus();
+//    public void setStatus(Boolean status);
+//}
+
+//public interface ISpeaker
+//{
+//    public int getVolume();
+//    public void setVolume(int volume);
+//}
+
+//public interface ILight
+//{
+//    public Color getColor();
+//    public void setColor(Color color);
+//}
 
 public abstract class ADevice
 {
@@ -59,27 +80,49 @@ public abstract class ADevice
     public void setStatus(Boolean status) { this.status = status; }
 }
 
-public class Speaker : ADevice, IDevice
+public class Speaker : ADevice
 {
     private int volume;
 
+    public Speaker()
+    {
+        this.volume = 50;
+    }
+
     public int getVolume() { return this.volume; }
-    public void setVolume(int volume) { this.volume = volume; }
+    public void setVolume(int volume)
+    {
+        if (volume >= 100)
+            this.volume = 100;
+        else if (volume <= 0)
+            this.volume = 0;
+        else
+            this.volume = volume;
+    }
 }
 
-public class Light : ADevice, IDevice
+public class Light : ADevice
 {
     private Color color;
     private int brightnessLevel;
 
+    public Light()
+    {
+        this.color = Color.WhiteSmoke;
+        this.brightnessLevel = 50;
+    }
+
     public Color getColor() { return color; }
     public void setColor(Color color) { this.color = color; }
+
+    public int getBrightness() { return this.brightnessLevel; }
+    public void setBrightness(int brightnessLevel) => this.brightnessLevel = brightnessLevel;
 }
 
 public enum ThermostatStatus { off, heat, cool}
 public enum OutSideWeather { sunny, cloudy, rainy, storm, apocalypse }
 
-public class Thermostat : ADevice, IDevice
+public class Thermostat : ADevice
 {
     private int insideTemp;
     private int outsideTemp;
@@ -100,34 +143,57 @@ public class Thermostat : ADevice, IDevice
     public OutSideWeather GetOutSideWeather() { return this.outSideWeather; }
 }
 
-public class SmartTV : ADevice, IDevice
+public class SmartTV : ADevice
 {
     private int channel;
     private Speaker speaker;
+
+    public SmartTV()
+    {
+        this.channel = 0;
+        this.speaker = new Speaker();
+    }
 
     public void setVolume(int volume) { speaker.setVolume(volume); }
     public void setChannel(int channel) { this.channel = channel; }
 }
 
-public class GateController : ADevice, IDevice
+public class GateController : ADevice
 {
     private Boolean closed;
+
+    public GateController()
+    {
+        this.closed = false;
+    }
 
     public void toggle() { this.closed = !this.closed; }
     public Boolean isClosed() { return this.closed; }
 }
 
-public class Camera : ADevice, IDevice
+public class Camera : ADevice
 {
     private Boolean recording;
+
+    public Camera()
+    {
+        this.recording = false;
+    }
+
     public void toggle() => this.recording = !this.recording;
     public Boolean isRecording() { return this.recording; }
 }
 
-public class Outlet : ADevice, IDevice
+public class Outlet : ADevice
 {
     //powered is true if powering other device
     private Boolean powered;
+
+    public Outlet()
+    {
+        this.powered = false;
+    }
+
     public void toggle() => this.powered = !this.powered;
     public Boolean isPowered() { return this.powered; }
 }
