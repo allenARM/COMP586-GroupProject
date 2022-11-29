78a4f825c2ac4d5f2fcc44839af9bcfcd556cf88
Ayvalex <114042284+Ayvalex@users.noreply.github.com>
2022-11-28 21:21:26 -0800

    Added remove device and some other changes.

--- 586FinalProject/Program.cs
+++ 586FinalProject/Program.cs
@@ -4,64 +4,114 @@ int main()
 {
     //IDevice house = new House("House");
 
-    House home = new House("House1");
+    House home = new House("HouseOne");
 
-    home.GetController().AddNewDevice(new Speaker(), home.GetRooms(), "Hallway");
+    //Living Room 
+    home.GetController().AddNewDevice(new Speaker(), home.GetRooms(), "LivingRoom");
+    home.GetController().AddNewDevice(new Speaker(), home.GetRooms(), "LivingRoom");
+    home.GetController().AddNewDevice(new SmartTV(), home.GetRooms(), "LivingRoom");
+    home.GetController().AddNewDevice(new Light(), home.GetRooms(), "LivingRoom");
+    home.GetController().AddNewDevice(new Thermostat(), home.GetRooms(), "LivingRoom");
+    home.GetController().AddNewDevice(new Outlet(), home.GetRooms(), "LivingRoom");
+
+    //Kitchen 
+    home.GetController().AddNewDevice(new Speaker(), home.GetRooms(), "Kitchen");
+    home.GetController().AddNewDevice(new Light(), home.GetRooms(), "Kitchen");
+    home.GetController().AddNewDevice(new Thermostat(), home.GetRooms(), "Kitchen");
+    home.GetController().AddNewDevice(new Outlet(), home.GetRooms(), "Kitchen");
+
+    //Bedroom
+    home.GetController().AddNewDevice(new Speaker(), home.GetRooms(), "Bedroom");
+    home.GetController().AddNewDevice(new Lock(), home.GetRooms(), "Bedroom");
+    home.GetController().AddNewDevice(new SmartTV(), home.GetRooms(), "Bedroom");
+    home.GetController().AddNewDevice(new Light(), home.GetRooms(), "Bedroom");
+    home.GetController().AddNewDevice(new Camera(), home.GetRooms(), "Bedroom");
+    home.GetController().AddNewDevice(new Thermostat(), home.GetRooms(), "Bedroom");
+    home.GetController().AddNewDevice(new Outlet(), home.GetRooms(), "Bedroom");
 
     //home.GetController().CheckIfExists(new Speaker());
 
-    Speaker speaker = (Speaker)home.GetRooms()[0].GetDevices()[0];
+    Speaker speakerOne = (Speaker)home.GetRooms()[0].GetDevices()[0];
+    Speaker speakerTwo = (Speaker)home.GetRooms()[0].GetDevices()[1];
+    SmartTV smartTV = (SmartTV)home.GetRooms()[0].GetDevices()[2];
+    Light light = (Light)home.GetRooms()[0].GetDevices()[3];
+    Thermostat thermostat = (Thermostat)home.GetRooms()[0].GetDevices()[4];
+    Outlet outlet = (Outlet)home.GetRooms()[0].GetDevices()[5];
 
-    Console.WriteLine(speaker.Volume);
+    //Console.WriteLine(home.GetRooms()[0].GetDevices());
+
+    Console.WriteLine($"{speakerOne.Location} {speakerOne.Volume} {speakerTwo.Volume} {smartTV.Channel} {light.Brightness} {thermostat.OutsideTemp} {outlet.IsPowered()}");
+
+    //Console.WriteLine((Outlet)home.GetRooms()[0].GetDevices()[5])
+
+    speakerOne.Volume = 78;
+    speakerOne.Volume = 13;
+    smartTV.Channel = 42;
+    light.Brightness = 91;
+    thermostat.OutsideTemp = 100;
+    outlet.Toggle();
+
+    Console.WriteLine($"{speakerOne.Location} {speakerOne.Volume} {speakerTwo.Volume} {smartTV.Channel} {light.Brightness} {thermostat.OutsideTemp} {outlet.IsPowered()}");
+
+    home.GetController().RemoveDevice(outlet, home.GetRooms());
+
+    Console.WriteLine($"{speakerOne.Location} {speakerOne.Volume} {speakerTwo.Volume} {smartTV.Channel} {light.Brightness} {thermostat.OutsideTemp} {outlet.IsPowered()}");
+
+    /*speakerOne = (Speaker)home.GetRooms()[1].GetDevices()[0];
+    light = (Light)home.GetRooms()[1].GetDevices()[1];
+    thermostat = (Thermostat)home.GetRooms()[1].GetDevices()[2];
+    outlet = (Outlet)home.GetRooms()[1].GetDevices()[3];
+
+    Console.WriteLine(speakerOne.GetLocation() + " " + speakerOne.Volume + " " + light.Brightness + " " + thermostat.OutsideTemp + " " + outlet.IsPowered());*/
+
+    /*Console.WriteLine(speakerOne.Volume);
     speaker.Volume = 99;
 
     speaker = (Speaker)home.GetRooms()[0].GetDevices()[0];
     Console.WriteLine(speaker.Volume);
-    Console.WriteLine(speaker.GetLocation());
+    Console.WriteLine(speaker.GetLocation());*/
+
+
     return 0;
 }
 
 main();
 
+
 public class Controller
 {
-    //List<IDevice> devices = new();
-    //List<Room> rooms = new();
-
-    //public List<IDevice> GetDevices() => devices;
-    //public List<Room> GetRooms() => rooms;
-
-    /*public Boolean CheckIfExists(object device)
-    {
-        foreach (object d in devices)
-        {
-            if (device.GetType() == d.GetType())
-                return true;
-        }
-        return false;
-    }*/
-
     public void AddNewDevice(IDevice device, List<Room> rooms, string location)
     {
         Boolean roomExists = false;
-
-        device.SetLocation(location);
+        
+        device.Location = location;
 
         foreach (var room in rooms)
         {
-            if (room.GetLocation() == location)
+            if (room.Location == location)
             {
                 room.AddDevice(device);
                 roomExists = true;
             }
         }
 
-        if (!roomExists)
+        if(!roomExists)
         {
             Room newRoom = new();
-            newRoom.SetLocation(location);
+            newRoom.Location = location;
             newRoom.AddDevice(device);
             rooms.Add(newRoom);
+        }
+    }
+
+    public void RemoveDevice(IDevice device, List<Room> rooms)
+    {
+        foreach (var room in rooms)
+        {
+            if (device.Location == room.Location)
+            {
+                room.RemoveDevice(device);
+            }
         }
     }
 }
@@ -74,12 +124,17 @@ public class Room
     public Room()
     {
         devices = new();
-        location = null;
+        location = string.Empty;
     }
-
-    public void SetLocation(string location) => this.location = location;
-    public string? GetLocation() => location;
+    public string? Location
+    {
+        get => location;
+        set => location = value;
+    }
+    //public void SetLocation(string location) => this.location = location;
+    //public string? GetLocation() => location;
     public void AddDevice(IDevice device) => devices.Add(device);
+    public void RemoveDevice(IDevice device) => devices.Remove(device);
     public List<IDevice> GetDevices() => devices;
 }
 
@@ -95,34 +150,42 @@ public class House
         this.houseName = houseName;
     }
 
-    public void SetHouseName(string houseName) { this.houseName = houseName; }
-    public string GetHouseName() => houseName;
+    public string HouseName
+    {
+        get => houseName;
+        set => houseName = value;
+    }
     public Controller GetController() => controller;
     public List<Room> GetRooms() => rooms;
 }
 
 public interface IDevice
 {
-    public string GetLocation();
-    public void SetLocation(string location);
+    public Boolean Status { get; set; }
+    public string Location { get; set; }
 }
 
-public abstract class ADevice
+public abstract class Device : IDevice
 {
-    private string location = "default";
     //if status true means device is on
     //if status false means device is off
     private Boolean status;
 
-
-    public string GetLocation() => location;
-    public void SetLocation(string location) => this.location = location;
-
-    public Boolean GetStatus() => status;
-    public void SetStatus(Boolean status) => this.status = status;
+    private string location = string.Empty;
+  
+    public Boolean Status
+    {
+        get => status;
+        set => status= value;
+    }
+    public string Location
+    {
+        get => location;
+        set => location = value;
+    }
 }
 
-public class Speaker : ADevice, IDevice
+public class Speaker : Device
 {
     private int volume;
 
@@ -154,7 +217,7 @@ public class Speaker : ADevice, IDevice
     }
 }
 
-public class Light : ADevice, IDevice
+public class Light : Device
 {
     private Color color;
     private int brightness;
@@ -177,9 +240,9 @@ public class Light : ADevice, IDevice
 }
 
 public enum ThermostatStatus { off, heat, cool }
-public enum OutSideWeather { sunny, cloudy, rainy, storm, apocalypse }
+public enum OutSideWeather { sunny, cloudy, clear, rain, storm }
 
-public class Thermostat : ADevice, IDevice
+public class Thermostat : Device
 {
     private int insideTemp;
     private int outsideTemp;
@@ -227,7 +290,7 @@ public class Thermostat : ADevice, IDevice
     }
 }
 
-public class SmartTV : ADevice, IDevice
+public class SmartTV : Device
 {
     private int channel;
     private Speaker speaker;
@@ -238,29 +301,32 @@ public class SmartTV : ADevice, IDevice
         speaker = new();
     }
 
-    public void SetVolume(int volume) => speaker.Volume = volume;
+    public int Volume
+    {
+        get => speaker.Volume;
+        set => speaker.Volume = value;
+    }
     public int Channel
     {
         get => channel;
         set => channel = value;
     }
-    //public void SetChannel(int channel) => this.channel = channel; 
 }
 
-public class Gate : ADevice, IDevice
+public class Lock : Device
 {
     private Boolean closed;
 
-    public Gate()
+    public Lock()
     {
         closed = false;
     }
 
-    public void Toggle() => closed = !closed;
-    public Boolean IsClosed() => closed;
+    public void Toggle() => closed = !closed; 
+    public Boolean IsClosed() => closed; 
 }
 
-public class Camera : ADevice, IDevice
+public class Camera : Device
 {
     private Boolean recording;
 
@@ -270,10 +336,10 @@ public class Camera : ADevice, IDevice
     }
 
     public void Toggle() => recording = !recording;
-    public Boolean IsRecording() => recording;
+    public Boolean IsRecording() => recording; 
 }
 
-public class Outlet : ADevice, IDevice
+public class Outlet : Device
 {
     //powered is true if powering other device
     private Boolean powered;
